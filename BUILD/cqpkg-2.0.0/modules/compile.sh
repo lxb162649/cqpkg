4621438d9080dc0d8d51b8c30f87056cccd5a730
lixuebing <lixuebing@cqsoftware.com.cn>
2025-07-09 11:00:25 +0800

    修复compile

--- BUILD/cqpkg-2.0.0/modules/compile.sh
+++ BUILD/cqpkg-2.0.0/modules/compile.sh
@@ -5,10 +5,10 @@ module_rpmbuild(){
     if [[ "$build_type" == *"$type"* ]]; then
         rpmbuild -$type -D "_topdir $PKG_PATH" "$SPEC_FILE"
         if [[ $? -ne 0 ]]; then
-            log_error "错误：执行失败！"
+            log_error "错误：$type 执行失败！"
             handle_interrupt
         fi
-        log_success "执行成功！"
+        log_success "$type 执行成功！"
     fi
 }
 # 编译
@@ -27,17 +27,19 @@ module_compile() {
     module_rpmbuild "$build_type" "bp"
     module_rpmbuild "$build_type" "bs"
 
-    log_info "\n构建结果："
-    for arch in noarch x86_64; do
-        local arch_dir="$RPM_DIR/$arch"
-        echo -e "${BLUE}== $arch ==${NC}"
-        if [ "$(ls "$arch_dir" 2>/dev/null)" ]; then
-            for rpm_file_path in "$arch_dir"/*; do
-                echo "$rpm_file_path"
-            done
-        else
-            echo "无"
-        fi
-        echo
-    done
+    if [[ "$build_type" == *"ba"* ]]; then
+        log_info "\n构建结果："
+        for arch in noarch x86_64; do
+            local arch_dir="$RPM_DIR/$arch"
+            echo -e "${BLUE}== $arch ==${NC}"
+            if [ "$(ls "$arch_dir" 2>/dev/null)" ]; then
+                for rpm_file_path in "$arch_dir"/*; do
+                    echo "$rpm_file_path"
+                done
+            else
+                echo "无"
+            fi
+            echo
+        done
+    fi
 }
--- SPECS/cqpkg_manager.spec
+++ SPECS/cqpkg_manager.spec
@@ -2,13 +2,14 @@
 
 Name:           cqpkg
 Version:        2.0.0
-Release:        2
+Release:        3
 Summary:        Manage CQ system software packages.
 
 License:        GPLv3+
 URL:            https://github.com/lxb162649/cqpkg
 Source0:        %{name}-%{version}.tar.gz
 Patch0:         cqos-fix-clone-generate_metadata.patch
+Patch1:         cqos-fix-compile.patch
 
 Requires:  git 
 Requires:  yum-utils
@@ -39,6 +40,9 @@ install -p -D -m 644 %{_builddir}/%{name}-%{version}/share/man/zh_CN/man1/* %{bu
 %{_mandir}/zh_CN/man1/*
 
 %changelog
+* Wed Jul 09 2025 lixuebing <lixuebing@cqsoftware.com.cn> - 2.0.0-3
+- 修复compile
+
 * Mon Jul 07 2025 lixuebing <lixuebing@cqsoftware.com.cn> - 2.0.0-2
 - 修复生成元数据文件函数
 
